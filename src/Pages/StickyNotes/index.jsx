228af76f55bf1a42262dfa7de0bcbb2198dd2fbe
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-03-27 00:05:14 +0200

    organizing folders and setting routes

---
+++ src/Pages/StickyNotes/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const StickyNotes = () => {
+  return <div>Sticky notes</div>;
+};
+
+export default StickyNotes;
---
+++ src/Pages/WeatherApp/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+
+const WeatherApp = () => {
+  return <div>Weather</div>;
+};
+
+export default WeatherApp;
